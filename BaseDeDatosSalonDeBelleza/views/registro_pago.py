301b27932ea56bd9fc848c0217883ed6dc51bbeb
Diego De Labra <diegolunadelabre@gmail.com>
2025-04-16 16:19:00 -0600

    Layout de botones de accion creado!

--- BaseDeDatosSalonDeBelleza/views/registro_pago.py
+++ BaseDeDatosSalonDeBelleza/views/registro_pago.py
@@ -31,11 +31,62 @@ class RegistroPago(QWidget):
         titulo_label.setAlignment(Qt.AlignCenter)
         titulo_layout.addWidget(titulo_label)
 ####################################################################
+###Layout de botones de Acción (Buscar y eliminar transacción por ID)###
+        layout_botones = QHBoxLayout()
+        boton_buscar=QPushButton("Buscar transacción")
+        boton_buscar.setStyleSheet("""
+            QPushButton {
+                background-color: #D9FFCC;
+                color: black;
+                border-radius: 20px;
+                padding: 12px 20px;
+                font-size: 16px;
+                font-weight: bold;
+                border: none;
+            }
+            QPushButton:hover {
+                background-color: lightblue;
+            }
+        """)
+
+        boton_eliminar=QPushButton("Eliminar transacción")
+        boton_eliminar.setStyleSheet("""
+            QPushButton {
+                background-color: #CF6978;
+                color: black;
+                border-radius: 20px;
+                padding: 12px 20px;
+                font-size: 16px;
+                font-weight: bold;
+                border: none;
+            }
+            QPushButton:hover {
+                background-color: lightblue;
+            }
+        """)
+        self.input_buscar = QLineEdit()
+        self.input_buscar.setPlaceholderText("Ingrese el ID por buscar")
+        self.input_buscar.setStyleSheet("background-color:white;font-size:20px;")
+        self.input_buscar.setMaximumWidth(225)
+
+        self.input_eliminar = QLineEdit()
+        self.input_eliminar.setPlaceholderText("Ingrese el ID por eliminar")
+        self.input_eliminar.setStyleSheet("background-color:white;font-size:20px;")
+        self.input_eliminar.setMaximumWidth(240)
+
+        layout_botones.addWidget(boton_buscar)
+        layout_botones.addWidget(self.input_buscar)
+        layout_botones.addWidget(boton_eliminar)
+        layout_botones.addWidget(self.input_eliminar)
+
 
 ###Área de layouts###
         main_layout.addLayout(titulo_layout)        
-        main_layout.setContentsMargins(0, 0, 0, 0)
-        main_layout.addStretch(1)
+        main_layout.addStretch(0)
+        main_layout.setSpacing(10)
+        main_layout.addLayout(layout_botones)
+        main_layout.addStretch(32)
+        main_layout.setSpacing(10)
         self.setLayout(main_layout)
         
 ####################################################################
