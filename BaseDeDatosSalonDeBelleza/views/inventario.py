1865b6956dc5a45ff87483bf797d5f7724500932
Diego De Labra <diegolunadelabre@gmail.com>
2025-04-16 01:21:03 -0600

    Boton y espacio para buscar productos por medio de su ID

--- BaseDeDatosSalonDeBelleza/views/inventario.py
+++ BaseDeDatosSalonDeBelleza/views/inventario.py
@@ -2,9 +2,9 @@
 import sys
 from PyQt5.QtWidgets import (
     QApplication,QWidget,QLabel,
-    QLineEdit,QPushButton,QVBoxLayout,QHBoxLayout, QTableWidget, QTableWidgetItem)
+    QLineEdit,QPushButton,QVBoxLayout,QHBoxLayout, QTableWidget, QTableWidgetItem,QSpacerItem, QSizePolicy)
 from PyQt5.QtGui import QFont, QPixmap, QIcon
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,QSize 
 from PyQt5 import QtGui
 
 class InventarioVentana(QWidget):
@@ -18,16 +18,17 @@ class InventarioVentana(QWidget):
                                                         stop: 0 pink,
                                                         stop: 1 white
                                                                     );""")
-        self.setWindowIcon(QIcon("resources/logoBD.ico"))
+        self.setWindowIcon(QIcon("resources/logoBD.jpg"))
         
         main_layout = QVBoxLayout()
-        ###Layout del titulo###
+###Layout del titulo####
         titulo_acciones_layout=QHBoxLayout()
         titulo_label=QLabel("Inventario")
         titulo_label.setStyleSheet("color:black; font-size:30px;font:bold")
         titulo_label.setAlignment(Qt.AlignCenter)
         titulo_acciones_layout.addWidget(titulo_label)
-        ###Layout de los botones y modificacion visual de los botones###
+####################################################################
+###Layout de los botones y modificacion visual de los botones###
         top_buttons_layout=QHBoxLayout()
         button_add = QPushButton("Agregar Producto")
         button_modificar = QPushButton("Modificar Producto")
@@ -80,13 +81,44 @@ class InventarioVentana(QWidget):
         top_buttons_layout.addWidget(button_add)
         top_buttons_layout.addWidget(button_modificar)
         top_buttons_layout.addWidget(button_delete)
-        ####################################################################
+####################################################################
+###Layout de busqueda de items###
+        layout_busqueda = QHBoxLayout()
+        layout_busqueda.addItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)) ##PARA EMPUJAR HACIA LA DERECHA
+        input_busqueda=QLineEdit()
+        input_busqueda.setStyleSheet("background-color:white;font-size:20px;")
+        input_busqueda.setFixedWidth(500)
+        input_busqueda.setPlaceholderText("Ingrese el id (1,2,3,..) del producto que desea buscar")
+        input_busqueda.setAlignment(Qt.AlignLeft)
+
+        boton_buscar=QPushButton("")
+        boton_buscar.setIcon(QIcon('C:/Users/Lutec/OneDrive/Documentos/Diego Luna De Labra/6to semestre/Bases de datos/BaseDeDatos/BaseDeDatosSalonDeBelleza/resources/lupa_sinfondo.png'))
+        boton_buscar.setIconSize(QSize(50,50))
+        boton_buscar.setFixedSize(30, 30)  # Establece el tamaño fijo en 100x100 píxeles
+        boton_buscar.setStyleSheet("""
+                                    QPushButton {
+                                    border: none;
+                                    background-color: transparent;
+                                    padding: 0px;
+                                    }
+                                    QPushButton:pressed {
+                                    background-color: transparent;
+                                    }
+                                    """)
+        layout_busqueda.addWidget(boton_buscar)
+
+        layout_busqueda.addWidget(input_busqueda)
+        
+
+
+####################################################################
         main_layout.addLayout(titulo_acciones_layout)
         main_layout.setContentsMargins(0, 0, 0, 0)
         main_layout.setSpacing(0)
-
         main_layout.addLayout(top_buttons_layout)
-        main_layout.addStretch()
+        main_layout.addSpacing(10)
+        main_layout.addLayout(layout_busqueda)
+        main_layout.addStretch(0)
         self.setLayout(main_layout)
 
 
